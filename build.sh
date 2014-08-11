10acdd8b50aa05bd178ac9ec7745442dcf311be7
Warren R Bank <warren.r.bank@gmail.com>
2014-08-10 20:01:19 -0700

    new screenshot

--- build.sh
+++
@@ -1,1 +0,0 @@
-zip -r HTTP-Archive-Viewer.xpi chrome.manifest install.rdf LICENSE README.md chrome components defaults
--- defaults/preferences/HTTP-Archive-Viewer.js
+++
@@ -1,8 +0,0 @@
-pref('extensions.HTTP_Archive_Viewer.validate_json',					true);
-pref('extensions.HTTP_Archive_Viewer.show_timeline',					true);
-pref('extensions.HTTP_Archive_Viewer.show_stats',						true);
-pref('extensions.HTTP_Archive_Viewer.expand_all_pages',					false);
-pref('extensions.HTTP_Archive_Viewer.expand_first_page',				true);
-pref('extensions.HTTP_Archive_Viewer.request_list.visible_columns',		"url,status,size,timeline");
-pref('extensions.HTTP_Archive_Viewer.request_list.phase_interval',		4000);
-pref('extensions.HTTP_Archive_Viewer.request_body.html_preview_height',	100);
